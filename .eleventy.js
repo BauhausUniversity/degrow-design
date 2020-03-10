3bed4f7178edf76b7ff68727a539ebbd04b69a84
kenokenobingo <mail.kewe@gmail.com>
2020-03-10 14:42:41 +0100

    Changed 404 permalink to a proper location

--- .eleventy.js
+++ .eleventy.js
@@ -16,7 +16,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
       callbacks: {
         ready: function(err, browserSync) {
-          const content_404 = fs.readFileSync('_site/404/index.html');
+          const content_404 = fs.readFileSync('_site/404.html');
   
           browserSync.addMiddleware('*', (req, res) => {
             // Provides the 404 content without redirect.
